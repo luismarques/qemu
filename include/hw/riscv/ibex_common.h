379db2873b3fd5a4cf06f1e38a0aa998bbdb7486
Emmanuel Blot <eblot@rivosinc.com>
2023-08-25 11:26:15 +0200

    [ot] hw/riscv: ibex_common: ensure memmap flag is unsigned
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

--- include/hw/riscv/ibex_common.h
+++ include/hw/riscv/ibex_common.h
@@ -122,7 +122,7 @@ struct IbexDeviceDef {
 #define IBEX_MEMMAP_REGIDX_MASK \
     ((IBEX_MEMMAP_REGIDX_COUNT)-1u) /* address are always word-aligned */
 #define IBEX_MEMMAP_MAKE_REG(_addr_, _flag_) \
-    ((_addr_) | ((_flag_)&IBEX_MEMMAP_REGIDX_MASK))
+    ((_addr_) | (((uint32_t)_flag_)&IBEX_MEMMAP_REGIDX_MASK))
 #define IBEX_MEMMAP_GET_REGIDX(_addr_)  ((_addr_)&IBEX_MEMMAP_REGIDX_MASK)
 #define IBEX_MEMMAP_GET_ADDRESS(_addr_) ((_addr_) & ~IBEX_MEMMAP_REGIDX_MASK)
 

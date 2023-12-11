ed28c7742c3d97fcdbc02f9f665f53b822bc1d75
Emmanuel Blot <eblot@rivosinc.com>
2023-12-11 17:09:36 +0100

    [ot] hw/riscv: create RISC-V Debug Module Interface
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

---
+++ hw/riscv/dmi.c
@@ -0,0 +1,524 @@
+/*
+ * QEMU Debug Module Interface and Controller
+ *
+ * Copyright (c) 2022-2023 Rivos, Inc.
+ * Author(s):
+ *  Emmanuel Blot <eblot@rivosinc.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Generated code for absract commands has been extracted from the PULP Debug
+ * module whose file (dm_mem.sv) contains the following copyright. This piece of
+ * code is self contained within the `riscv_dmi_dm_access_register` function:
+ *
+ *   Copyright and related rights are licensed under the Solderpad Hardware
+ *   License, Version 0.51 (the “License”); you may not use this file except in
+ *   compliance with the License. You may obtain a copy of the License at
+ *   http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+ *   or agreed to in writing, software, hardware and materials distributed under
+ *   this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ */
+
+#include "qemu/osdep.h"
+#include "qemu/bitmap.h"
+#include "qemu/compiler.h"
+#include "qemu/guest-random.h"
+#include "qemu/log.h"
+#include "qemu/main-loop.h"
+#include "qemu/timer.h"
+#include "qemu/typedefs.h"
+#include "qapi/error.h"
+#include "disas/dis-asm.h"
+#include "exec/address-spaces.h"
+#include "exec/cpu_ldst.h"
+#include "exec/jtagstub.h"
+#include "hw/boards.h"
+#include "hw/core/cpu.h"
+#include "hw/irq.h"
+#include "hw/qdev-properties-system.h"
+#include "hw/qdev-properties.h"
+#include "hw/registerfields.h"
+#include "hw/riscv/debug.h"
+#include "hw/riscv/dmi.h"
+#include "hw/sysbus.h"
+#include "sysemu/cpus.h"
+#include "sysemu/hw_accel.h"
+#include "sysemu/runstate.h"
+#include "target/riscv/cpu.h"
+#include "trace-target_riscv.h"
+#include "trace.h"
+
+
+/*
+ * Register definitions
+ */
+
+/* clang-format off */
+
+/* Debug Module Interface and Control */
+REG32(DTMCS, 0x10u)
+    FIELD(DTMCS, VERSION, 0u, 4u)
+    FIELD(DTMCS, ABITS, 4u, 6u)
+    FIELD(DTMCS, DMISTAT, 10u, 2u)
+    FIELD(DTMCS, IDLE, 12u, 3u)
+    FIELD(DTMCS, DMIRESET, 16u, 1u)
+    FIELD(DTMCS, DMIHARDRESET, 17u, 1u)
+REG64(DMI, 0x11u)
+    FIELD(DMI, OP, 0u, 2u)
+    FIELD(DMI, DATA, 2u, 32u)
+    FIELD(DMI, ADDRESS, 34u, 64u-34u) /* real width is a runtime property */
+
+/*
+ * Macros
+ */
+
+#define xtrace_riscv_dmi_error(_msg_) \
+    trace_riscv_dmi_error(__func__, __LINE__, _msg_)
+#define xtrace_riscv_dmi_info(_msg_, _val_) \
+    trace_riscv_dmi_info(__func__, __LINE__, _msg_, _val_)
+
+/*
+ * Type definitions
+ */
+
+typedef QLIST_HEAD(, RISCVDebugModule) RISCVDebugModuleList;
+
+typedef struct RISCVDebugModule {
+    QLIST_ENTRY(RISCVDebugModule) entry;
+    RISCVDebugDeviceState *dev;
+    RISCVDebugDeviceClass *dc;
+    uint32_t base;
+    uint32_t size;
+} RISCVDebugModule;
+
+/** Debug Module Interface */
+struct RISCVDMIState {
+    DeviceState parent;
+
+    RISCVDebugModuleList dms;
+    RISCVDebugModule *last_dm; /* last selected DM */
+
+    uint32_t address; /* last updated address */
+    RISCVDebugResult dmistat; /* Operation result */
+    bool cmd_busy; /* A command is being executed */
+    bool jtag_ok; /* JTAG server initialized */
+
+    /* properties */
+    unsigned tap_ir_length;
+    unsigned tap_idcode;
+    unsigned abits; /* address bit count */
+};
+
+/*
+ * Forward declarations
+ */
+
+static void riscv_dmi_reset(DeviceState *dev);
+static RISCVDebugModule* riscv_dmi_get_dm(RISCVDMIState *s, uint32_t addr);
+static void riscv_dmi_sort_dms(RISCVDMIState *s);
+
+static void riscv_dmi_tap_dtmcs_capture(TAPDataHandler *tdh);
+static void riscv_dmi_tap_dtmcs_update(TAPDataHandler *tdh);
+static void riscv_dmi_tap_dmi_capture(TAPDataHandler *tdh);
+static void riscv_dmi_tap_dmi_update(TAPDataHandler *tdh);
+
+/*
+ * Constants
+ */
+
+#define RISCV_DEBUG_DMI_VERSION 1u /* RISC-V Debug spec 0.13.x & 1.0 */
+#define RISCVDMI_DTMCS_IR           0x10u
+#define RISCVDMI_DMI_IR             0x11u
+
+static const TAPDataHandler RISCVDMI_DTMCS = {
+    .name = "dtmcs",
+    .length = 32u,
+    .value = RISCV_DEBUG_DMI_VERSION, /* abits updated at runtime */
+    .capture = &riscv_dmi_tap_dtmcs_capture,
+    .update = &riscv_dmi_tap_dtmcs_update,
+};
+
+static const TAPDataHandler RISCVDMI_DMI = {
+    .name = "dmi",
+    /* data, op; abits updated at runtime */
+    .length = R_DMI_OP_LENGTH + R_DMI_DATA_LENGTH,
+    .capture = &riscv_dmi_tap_dmi_capture,
+    .update = &riscv_dmi_tap_dmi_update,
+};
+
+#define MAKE_RUNSTATE_ENTRY(_ent_) [RUN_STATE_##_ent_] = stringify(_ent_)
+static const char *RISCVDMI_RUNSTATE_NAMES[] = {
+    /* clang-format off */
+    MAKE_RUNSTATE_ENTRY(DEBUG),
+    MAKE_RUNSTATE_ENTRY(INMIGRATE),
+    MAKE_RUNSTATE_ENTRY(INTERNAL_ERROR),
+    MAKE_RUNSTATE_ENTRY(IO_ERROR),
+    MAKE_RUNSTATE_ENTRY(PAUSED),
+    MAKE_RUNSTATE_ENTRY(POSTMIGRATE),
+    MAKE_RUNSTATE_ENTRY(PRELAUNCH),
+    MAKE_RUNSTATE_ENTRY(FINISH_MIGRATE),
+    MAKE_RUNSTATE_ENTRY(RESTORE_VM),
+    MAKE_RUNSTATE_ENTRY(RUNNING),
+    MAKE_RUNSTATE_ENTRY(SAVE_VM),
+    MAKE_RUNSTATE_ENTRY(SHUTDOWN),
+    MAKE_RUNSTATE_ENTRY(SUSPENDED),
+    MAKE_RUNSTATE_ENTRY(WATCHDOG),
+    MAKE_RUNSTATE_ENTRY(GUEST_PANICKED),
+    MAKE_RUNSTATE_ENTRY(COLO),
+    /* clang-format on */
+};
+#undef MAKE_RUNSTATE_ENTRY
+#define RUNSTATE_NAME(_reg_) \
+    ((((_reg_) <= ARRAY_SIZE(RISCVDMI_RUNSTATE_NAMES)) && \
+      RISCVDMI_RUNSTATE_NAMES[_reg_]) ? \
+         RISCVDMI_RUNSTATE_NAMES[_reg_] : \
+         "?")
+
+/* -------------------------------------------------------------------------- */
+/* Public API */
+/* -------------------------------------------------------------------------- */
+
+bool riscv_dmi_register_dm(DeviceState *dev, RISCVDebugDeviceState *dbgdev,
+                           hwaddr base_addr, hwaddr size)
+{
+    RISCVDMIState *s = RISCV_DMI(dev);
+
+    if ((base_addr + size - 1u) > (1u << s->abits)) {
+        error_setg(&error_fatal,
+                   "DM address range cannot be encoded in %u address bits",
+                   s->abits);
+    }
+
+    RISCVDebugModule *node;
+    unsigned count = 0;
+    QLIST_FOREACH(node, &s->dms, entry) {
+        count += 1u;
+        if ((node->dev == dbgdev) && (node->base == base_addr) &&
+            (node->size == size)) {
+            /* already registered */
+            return s->jtag_ok;
+        }
+        if (base_addr > (node->base + node->size - 1u)) {
+            continue;
+        }
+        if ((base_addr + size - 1u) < node->base) {
+            continue;
+        }
+
+        error_setg(&error_fatal, "%s: Debug Module overlap\n", __func__);
+    }
+
+    object_ref(OBJECT(dbgdev));
+
+    RISCVDebugModule *dm = g_new(RISCVDebugModule, 1u);
+    dm->dev = dbgdev;
+    dm->dc = RISCV_DEBUG_DEVICE_GET_CLASS(OBJECT(dbgdev));
+    dm->base = base_addr;
+    dm->size = size;
+
+    QLIST_INSERT_HEAD(&s->dms, dm, entry);
+    s->last_dm = dm;
+
+    trace_riscv_dmi_register_dm(count, base_addr, base_addr + size - 1u,
+                                s->jtag_ok);
+
+    riscv_dmi_sort_dms(s);
+
+    return s->jtag_ok;
+}
+
+/* -------------------------------------------------------------------------- */
+/* DTMCS/DMI implementation */
+/* -------------------------------------------------------------------------- */
+
+static void riscv_dmi_tap_dtmcs_capture(TAPDataHandler *tdh)
+{
+    RISCVDMIState *s = tdh->opaque;
+
+    tdh->value = (s->abits << 4u) | (RISCV_DEBUG_DMI_VERSION << 0u) |
+                 ((uint64_t)s->dmistat << 10u); /* see DMI op result */
+}
+
+static void riscv_dmi_tap_dtmcs_update(TAPDataHandler *tdh)
+{
+    RISCVDMIState *s = tdh->opaque;
+    if (tdh->value & (1u << 16u)) {
+        /* dmireset */
+        s->dmistat = RISCV_DEBUG_NOERR;
+    }
+    if (tdh->value & (1u << 17u)) {
+        /* dmi hardreset */
+        qemu_log_mask(LOG_UNIMP, "%s: DMI hard reset\n", __func__);
+    }
+}
+
+static void riscv_dmi_tap_dmi_capture(TAPDataHandler *tdh)
+{
+    RISCVDMIState *s = tdh->opaque;
+
+    uint32_t addr = s->address;
+    uint32_t value;
+
+    if (s->dmistat == RISCV_DEBUG_NOERR) {
+        RISCVDebugModule *dm = riscv_dmi_get_dm(s, addr);
+        if (!dm) {
+            s->dmistat = RISCV_DEBUG_FAILED;
+            value = 0;
+            qemu_log_mask(LOG_UNIMP, "%s: Unknown DM address 0x%x\n", __func__,
+                          addr);
+        } else {
+            value = dm->dc->read_value(dm->dev);
+        }
+    } else {
+        value = 0;
+    }
+
+    /*
+     * In Capture-DR, the DTM updates data with the result from [the previous
+     * update] operation, updating op if the current op isn’t sticky.
+     */
+    tdh->value = (((uint64_t)addr) << (32u + 2u)) | (((uint64_t)value) << 2u) |
+                 ((uint64_t)(s->dmistat & 0b11));
+}
+
+static void riscv_dmi_tap_dmi_update(TAPDataHandler *tdh)
+{
+    RISCVDMIState *s = tdh->opaque;
+
+    uint32_t value;
+    uint32_t addr =
+        (uint32_t)extract64(tdh->value, R_DMI_ADDRESS_SHIFT, (int)s->abits);
+    unsigned op = (unsigned)(tdh->value & 0b11);
+
+    /* store address for next read back */
+    s->address = addr;
+
+    RISCVDebugModule *dm = riscv_dmi_get_dm(s, addr);
+    if (!dm) {
+        s->dmistat = RISCV_DEBUG_FAILED;
+        qemu_log_mask(LOG_UNIMP, "%s: Unknown DM address 0x%x\n", __func__,
+                      addr);
+        return;
+    }
+
+    /*
+     * In Update-DR, the DTM starts the operation specified in op unless the
+     * current status reported in op is sticky.
+     */
+    switch (op) {
+    case 0: /* NOP */
+        /*
+         * Don’t send anything over the DMI during Update-DR. This operation
+         * should never result in a busy or error response. The address and data
+         * reported in the following Capture-DR are undefined.
+         */
+        return;
+    case 1: /* READ from address */
+        s->dmistat = dm->dc->read_rq(dm->dev, addr - dm->base);
+        break;
+    case 2: /* WRITE to address */
+        value = (uint32_t)FIELD_EX64(tdh->value, DMI, DATA);
+        s->dmistat = dm->dc->write_rq(dm->dev, addr - dm->base, value);
+        break;
+    case 3:
+    default:
+        s->dmistat = RISCV_DEBUG_FAILED;
+        qemu_log_mask(LOG_UNIMP, "%s: Unknown operation %u\n", __func__, op);
+    }
+}
+
+static void riscv_dmi_register_tap_handlers(RISCVDMIState *s)
+{
+    /*
+     * copy the template to update the opaque value
+     * no lifetime issue as the data handler is copied by the TAP controller
+     */
+    TAPDataHandler tdh;
+
+    memcpy(&tdh, &RISCVDMI_DTMCS, sizeof(TAPDataHandler));
+    tdh.value |= s->abits << 4u; /* add address bit count */
+    tdh.opaque = s;
+    if (jtag_register_handler(RISCVDMI_DTMCS_IR, &tdh)) {
+        xtrace_riscv_dmi_error("cannot register DMTCS");
+        return;
+    }
+
+    memcpy(&tdh, &RISCVDMI_DMI, sizeof(TAPDataHandler));
+    tdh.length += s->abits; /* add address bit count */
+    tdh.opaque = s;
+    /* the data handler is copied by the TAP controller */
+    if (jtag_register_handler(RISCVDMI_DMI_IR, &tdh)) {
+        xtrace_riscv_dmi_error("cannot register DMI");
+        return;
+    }
+}
+
+static RISCVDebugModule *riscv_dmi_get_dm(RISCVDMIState *s, uint32_t addr)
+{
+    RISCVDebugModule *dm = s->last_dm;
+
+    if (dm && (addr >= dm->base) && (addr < dm->base + dm->size)) {
+        return dm;
+    }
+
+    QLIST_FOREACH(dm, &s->dms, entry) {
+        if ((addr >= dm->base) && (addr < dm->base + dm->size)) {
+            s->last_dm = dm;
+            return dm;
+        }
+    }
+
+    s->last_dm = NULL;
+    return NULL;
+}
+
+static void riscv_dmi_vm_state_change(void *opaque, bool running,
+                                      RunState state)
+{
+    (void)opaque;
+    (void)running;
+    trace_riscv_dmi_vm_state_change(RUNSTATE_NAME(state), state);
+}
+
+static int riscv_dmi_order_dm(const void *pdm1, const void *pdm2)
+{
+    return ((int)(*(RISCVDebugModule **)pdm1)->base) -
+           ((int)(*(RISCVDebugModule **)pdm2)->base);
+}
+
+static void riscv_dmi_sort_dms(RISCVDMIState *s)
+{
+    RISCVDebugModule *dm;
+
+    /* iterate once to get the count of managed DMs */
+    unsigned count = 0;
+    QLIST_FOREACH(dm, &s->dms, entry) {
+        count += 1;
+    }
+
+    /* create an array of sortable DM references */
+    RISCVDebugModule **dma = g_new(RISCVDebugModule *, count);
+    RISCVDebugModule **cdm = dma;
+    QLIST_FOREACH(dm, &s->dms, entry) {
+        *cdm++ = dm;
+    }
+
+    /* sort DM reference by increasing base address */
+    qsort(dma, count, sizeof(RISCVDebugModule *), &riscv_dmi_order_dm);
+
+    /* create a new list of ordered, managed DMs */
+    RISCVDebugModuleList sorted;
+    QLIST_INIT(&sorted);
+    RISCVDebugModule *cur = QLIST_FIRST(&sorted);
+    for (unsigned ix = 0; ix < count; ix++) {
+        QLIST_REMOVE(dma[ix], entry);
+        if (!cur) {
+            QLIST_INSERT_HEAD(&sorted, dma[ix], entry);
+        } else {
+            QLIST_INSERT_AFTER(cur, dma[ix], entry);
+        }
+        cur = dma[ix];
+    }
+    /* replace current list head with the new ordered one */
+    s->dms = sorted;
+
+    g_free(dma);
+}
+
+static Property riscv_dmi_properties[] = {
+    DEFINE_PROP_UINT32("tap_ir_length", RISCVDMIState, tap_ir_length, 5u),
+    DEFINE_PROP_UINT32("tap_idcode", RISCVDMIState, tap_idcode, 0x0u),
+    DEFINE_PROP_UINT32("abits", RISCVDMIState, abits, 0x7u),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void riscv_dmi_reset(DeviceState *dev)
+{
+    RISCVDMIState *s = RISCV_DMI(dev);
+
+    s->address = 0;
+    s->last_dm = NULL;
+
+    if (!s->jtag_ok) {
+        /* JTAG not enabled, nothing more can be done for this session */
+        return;
+    }
+}
+
+static void riscv_dmi_realize(DeviceState *dev, Error **errp)
+{
+    RISCVDMIState *s = RISCV_DMI(dev);
+
+    if (s->tap_ir_length > 8u) {
+        error_setg(errp, "Unsupported IR length");
+        return;
+    }
+    if (s->tap_idcode == 0u) {
+        error_setg(errp, "Invalid IDCODE");
+        return;
+    }
+    if (s->abits < 7u || s->abits > 30u) {
+        error_setg(errp, "Invalid address bit count");
+        return;
+    }
+
+    /* may fail if no JTAG server is active */
+    s->jtag_ok = jtag_configure_tap(s->tap_ir_length, s->tap_idcode) == 0;
+
+    if (s->jtag_ok) {
+        (void)riscv_dmi_register_tap_handlers(s);
+    }
+}
+
+static void riscv_dmi_init(Object *obj)
+{
+    RISCVDMIState *s = RISCV_DMI(obj);
+
+    qemu_add_vm_change_state_handler(&riscv_dmi_vm_state_change, s);
+
+    QLIST_INIT(&s->dms);
+}
+
+static void riscv_dmi_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    (void)data;
+
+    dc->reset = &riscv_dmi_reset;
+    dc->realize = &riscv_dmi_realize;
+    device_class_set_props(dc, riscv_dmi_properties);
+    set_bit(DEVICE_CATEGORY_MISC, dc->categories);
+}
+
+static const TypeInfo riscv_dmi_info = {
+    .name = TYPE_RISCV_DMI,
+    .parent = TYPE_DEVICE,
+    .instance_size = sizeof(RISCVDMIState),
+    .instance_init = &riscv_dmi_init,
+    .class_init = &riscv_dmi_class_init,
+};
+
+static void riscv_dmi_register_types(void)
+{
+    type_register_static(&riscv_dmi_info);
+}
+
+type_init(riscv_dmi_register_types);
---
+++ include/hw/riscv/dmi.h
@@ -0,0 +1,50 @@
+/*
+ * QEMU RISC-V Debug Module Interface and Controller
+ *
+ * Copyright (c) 2022-2023 Rivos, Inc.
+ * Author(s):
+ *  Emmanuel Blot <eblot@rivosinc.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef HW_RISCV_DMI_H
+#define HW_RISCV_DMI_H
+
+#include "exec/hwaddr.h"
+#include "hw/riscv/debug.h"
+
+#define TYPE_RISCV_DMI "riscv-dmi"
+OBJECT_DECLARE_SIMPLE_TYPE(RISCVDMIState, RISCV_DMI)
+
+/**
+ * Register a debug module on the Debug Module Interface.
+ * It is valid to register the same module multiple time, as long as base_addr
+ * and size are not modified.
+ *
+ * @dev the DMI instance
+ * @dmif the DM to register
+ * @base_addr the address of the first DM register
+ * @size the count of DM registers
+ * @return @c true if DMI is enabled, @c false otherwise
+ */
+bool riscv_dmi_register_dm(DeviceState *dev, RISCVDebugDeviceState *dmif,
+                           hwaddr base_addr, hwaddr size);
+
+#endif /* HW_RISCV_DMI_H */

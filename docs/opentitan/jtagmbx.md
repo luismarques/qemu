fc63d01db578bc81da51ae15a7ed8947cfb2372e
Emmanuel Blot <eblot@rivosinc.com>
2024-02-26 12:39:17 +0100

    [ot] scripts/opentitan: add a mailbox requester JTAG implementation
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

--- docs/opentitan/jtagmbx.md
+++ docs/opentitan/jtagmbx.md
@@ -31,6 +31,46 @@ where:
 QEMU should be started with an option such as `-jtag tcp::3335` so that the JTAG server is
 instantiated and listen for incoming connection on TCP port 3335.
 
+#### macOS
+
+If you want to avoid the boring pop-up window from macOS
+```
+Do you want the application “qemu-system-riscv32” to accept incoming network connections?
+```
+restrict the listening interfaces to the localhost with `-jtag tcp:localhost:3335` as QEMU defaults
+to listening on all interfaces, _i.e._ 0.0.0.0
+
+## Communicating with JTAG server and JTAG MailBox using Python
+
+OpenTitan implementation provides JTAG/DTM/DMI/Mailbox stack available as Python modules:
+
+* jtag/tap module is available from `scripts/jtag` directory
+* dtm/dmi and jtag mailbox modules are available from `scripts/opentitan` directory
+
+Python snippet to create a communication channel with the VM JTAG mailbox:
+
+````py
+from socket import create_connection
+from jtag.bitbang import JtagBitbangController
+from jtag.jtag import JtagEngine
+from ot.dtm import DebugTransportModule
+from ot.mailbox.jtag import JtagMbox
+
+sock = create_connection(('localhost', 3335), timeout=1.0)
+ctrl = JtagBitbangController(sock)
+eng = JtagEngine(ctrl)
+ctrl.tap_reset(True)
+dtm = DebugTransportModule(eng, 5)  # IR length depends on the actual machine
+version, abits = dtm['dtmcs'].dmi_version, dtm['dtmcs'].abits
+print(f'DTM: v{version[0]}.{version[1]}, {abits} bits')
+dtm['dtmcs'].dmireset()
+mbj = JtagMbox(dtm, 0x2200 >> 2)
+
+# See ot.mailbox.sysmbox.SysMbox for mailbox communication API
+````
+
+## Communicating with JTAG server using OpenOCD
+
 It is possible from OpenOCD running a host to connect to the embedded JTAG server using the
 `remote_bitbang` protocol, using a configuration script such as
 
---
+++ scripts/opentitan/ot/mailbox/jtag.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2024 Rivos, Inc.
+# SPDX-License-Identifier: Apache2
+
+"""Mailbox over JTAG access.
+
+   :author: Emmanuel Blot <eblot@rivosinc.com>
+"""
+
+from logging import getLogger
+
+# see scripts/jtag
+# pylint: disable=import-error
+# pylint: disable=unused-import
+from jtag.jtag import JtagError  # noqa: F401
+from .sysmbox import SysMbox
+from ..dtm import DebugTransportModule
+
+
+class JtagMbox(SysMbox):
+    """JTAG Mailbox requester API
+    """
+
+    SYSREGS = dict(control=2, status=3, wdata=4, rdata=5)
+
+    def __init__(self, dtm: 'DebugTransportModule', address: int):
+        super().__init__()
+        self._log = getLogger('dtm.jtagmbx')
+        self._dmi = dtm['dmi']
+        self._address = address
+
+    @property
+    def busy(self) -> bool:
+        return bool(self._read_reg('status') & 0b001)
+
+    @property
+    def on_error(self) -> bool:
+        return bool(self._read_reg('status') & 0b100)
+
+    @property
+    def object_ready(self) -> bool:
+        return bool(self._read_reg('status') >> 31)
+
+    def go(self) -> None:
+        self._write_reg('control', 1 << 31)
+
+    def abort(self) -> None:
+        self._write_reg('control', 0b1)
+
+    def write_word(self, word: int) -> None:
+        self._write_reg('wdata', word)
+
+    def read_word(self, ack: bool = True) -> int:
+        word = self._read_reg('rdata')
+        if ack:
+            self.acknowledge_read()
+        return word
+
+    def acknowledge_read(self) -> None:
+        self._write_reg('rdata', 0)  # value is meaningless
+
+    def _write_reg(self, name: str, value: int) -> None:
+        if value >= (1 << 32):
+            raise ValueError('Invalid value')
+        try:
+            reg = self.SYSREGS[name]
+        except KeyError as exc:
+            raise ValueError(f"No such mailbox register: '{name}'") from exc
+        self._log.info('write %s: 0x%08x', name, value)
+        return self._dmi.write(self._address + reg, value)
+
+    def _read_reg(self, name: str) -> int:
+        try:
+            reg = self.SYSREGS[name]
+        except KeyError as exc:
+            raise ValueError(f"No such mailbox register: '{name}'") from exc
+        self._log.info('read %s', name)
+        value = self._dmi.read(self._address + reg)
+        self._log.info('read 0x%08x', value)
+        return value

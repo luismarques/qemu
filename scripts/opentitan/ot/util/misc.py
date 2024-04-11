f1b233de55d0bac407edbb9bdb102d583bc8f0f9
Emmanuel Blot <eblot@rivosinc.com>
2024-04-11 20:37:48 +0200

    [ot] scripts/opentitan: misc.py: update and fix dump_buffer
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

--- scripts/opentitan/ot/util/misc.py
+++ scripts/opentitan/ot/util/misc.py
@@ -6,7 +6,8 @@
    :author: Emmanuel Blot <eblot@rivosinc.com>
 """
 
-from typing import Any, Optional
+from sys import stdout
+from typing import Any, Optional, TextIO
 
 try:
     # only available from Python 3.12+
@@ -36,16 +37,26 @@ class HexInt(int):
         return int(val, val.startswith('0x') and 16 or 10)
 
 
-def dump_buffer(buffer: Buffer, addr: int) -> None:
+def dump_buffer(buffer: Buffer, addr: int = 0, file: Optional[TextIO] = None) \
+        -> None:
     """Dump a binary buffer, same format as hexdump -C."""
-    view = buffer.getbuffer()
+    if isinstance(buffer, memoryview):
+        view = buffer.getbuffer()
+    else:
+        view = buffer
     size = len(view)
+    if not file:
+        file = stdout
     for pos in range(0, size, 16):
         chunks = view[pos:pos+8], view[pos+8:pos+16]
         buf = '  '.join(' '.join(f'{x:02x}' for x in c) for c in chunks)
-        text = ''.join(chr(c) if 0x20 <= c < 0x7f else '.'
-                       for c in view[pos:pos+16])
-        print(f'{addr+pos:08x}  {buf}  |{text}|')
+        if len(buf) < 48:
+            buf = f'{buf}{" " * (48 - len(buf))}'
+        chunk = view[pos:pos+16]
+        text = ''.join(chr(c) if 0x20 <= c < 0x7f else '.' for c in chunk)
+        if len(text) < 16:
+            text = f'{text}{" " * (16-len(text))}'
+        print(f'{addr+pos:08x}  {buf}  |{text}|', file=file)
 
 
 def round_up(value: int, rnd: int) -> int:

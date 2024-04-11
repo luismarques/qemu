d8ae414950b2142efa56d642dd1e99a31f3cb15e
Emmanuel Blot <eblot@rivosinc.com>
2024-04-11 17:06:48 +0200

    [ot] scripts/opentitan: misc.py: add a dump buffer debug function.
    Output is similar to `hexdump -C`
    
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

--- scripts/opentitan/ot/util/misc.py
+++ scripts/opentitan/ot/util/misc.py
@@ -6,6 +6,15 @@
    :author: Emmanuel Blot <eblot@rivosinc.com>
 """
 
+from typing import Optional
+
+try:
+    # only available from Python 3.12+
+    from collections.abc import Buffer
+except ImportError:
+    Buffer = [bytes | bytearray | memoryview]
+
+
 class HexInt(int):
     """Simple wrapper to always represent an integer in hexadecimal format."""
 
@@ -13,6 +22,20 @@ class HexInt(int):
         return f'0x{self:x}'
 
     @staticmethod
-    def parse(val: str) -> int:
+    def parse(val: Optional[str]) -> Optional[int]:
         """Simple helper to support hexadecimal integer in argument parser."""
+        if val is None:
+            return None
         return int(val, val.startswith('0x') and 16 or 10)
+
+
+def dump_buffer(buffer: Buffer, addr: int) -> None:
+    """Dump a binary buffer, same format as hexdump -C."""
+    view = buffer.getbuffer()
+    size = len(view)
+    for pos in range(0, size, 16):
+        chunks = view[pos:pos+8], view[pos+8:pos+16]
+        buf = '  '.join(' '.join(f'{x:02x}' for x in c) for c in chunks)
+        text = ''.join(chr(c) if 0x20 <= c < 0x7f else '.'
+                       for c in view[pos:pos+16])
+        print(f'{addr+pos:08x}  {buf}  |{text}|')

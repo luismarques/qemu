8536ad2f91c1480e9457afa80dcdb4d16d5bbb28
Emmanuel Blot <eblot@rivosinc.com>
2024-05-23 19:32:49 +0200

    [ot] scripts/opentitan: log.py: enable fine grained log configuration
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

--- scripts/opentitan/ot/util/log.py
+++ scripts/opentitan/ot/util/log.py
@@ -6,14 +6,18 @@
    :author: Emmanuel Blot <eblot@rivosinc.com>
 """
 
-from logging import (Formatter, Logger, StreamHandler, CRITICAL, DEBUG, INFO,
-                     ERROR, WARNING, getLogger)
 from os import isatty
 from sys import stderr
 from typing import List, Tuple
 
+import logging
 
-class ColorLogFormatter(Formatter):
+
+_LEVELS = {k: v for k, v in logging.getLevelNamesMapping().items()
+           if k not in ('NOTSET', 'WARN')}
+
+
+class ColorLogFormatter(logging.Formatter):
     """Custom log formatter for ANSI terminals.
        Colorize log levels.
 
@@ -34,11 +38,11 @@ class ColorLogFormatter(Formatter):
     FMT_LEVEL = '%(levelname)8s'
 
     COLORS = {
-        DEBUG: GREY,
-        INFO: WHITE,
-        WARNING: YELLOW,
-        ERROR: RED,
-        CRITICAL: MAGENTA,
+        logging.DEBUG: GREY,
+        logging.INFO: WHITE,
+        logging.WARNING: YELLOW,
+        logging.ERROR: RED,
+        logging.CRITICAL: MAGENTA,
     }
 
     def __init__(self, *args, **kwargs):
@@ -69,12 +73,12 @@ class ColorLogFormatter(Formatter):
     def format(self, record):
         log_fmt = self._color_formats[record.levelno] if self._use_ansi \
                   else self._plain_format
-        formatter = Formatter(log_fmt, *self._formatter_args)
+        formatter = logging.Formatter(log_fmt, *self._formatter_args)
         return formatter.format(record)
 
 
 def configure_loggers(level: int, *lognames: List[str], **kwargs) \
-        -> List[Logger]:
+        -> List[logging.Logger]:
     """Configure loggers.
 
        :param level: level (stepping: 1)
@@ -82,21 +86,32 @@ def configure_loggers(level: int, *lognames: List[str], **kwargs) \
        :param kwargs: optional features
        :return: configured loggers or level change
     """
-    loglevel = ERROR - (10 * (level or 0))
-    loglevel = min(ERROR, loglevel)
+    loglevel = logging.ERROR - (10 * (level or 0))
+    loglevel = min(logging.ERROR, loglevel)
+    loglevels = {}
+    for lvl in _LEVELS:
+        lnames = kwargs.pop(lvl.lower(), None)
+        if lnames:
+            if isinstance(lnames, str):
+                lnames = [lnames]
+            loglevels[lvl] = tuple(lnames)
     formatter = ColorLogFormatter(**kwargs)
-    logh = StreamHandler(stderr)
+    logh = logging.StreamHandler(stderr)
     logh.setFormatter(formatter)
-    loggers: List[Logger] = []
-    logdefs: List[Tuple[List[str], Logger]] = []
+    loggers: List[logging.Logger] = []
+    logdefs: List[Tuple[List[str], logging.Logger]] = []
     for logdef in lognames:
         if isinstance(logdef, int):
             loglevel += -10 * logdef
             continue
-        log = getLogger(logdef)
-        log.setLevel(max(DEBUG, loglevel))
+        log = logging.getLogger(logdef)
+        log.setLevel(max(logging.DEBUG, loglevel))
         loggers.append(log)
         logdefs.append((logdef.split('.'), log))
+    for lvl, lnames in loglevels.items():
+        for lname in lnames:
+            log = logging.getLogger(lname)
+        log.setLevel(_LEVELS[lvl])
     logdefs.sort(key=lambda p: len(p[0]))
     # ensure there is only one handler per logger subtree
     for _, log in logdefs:
